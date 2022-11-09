ccd683b366bb86233d44171c97789a1b0574ff8b
M-Naill <mahmoud.naill7@gmail.com>
2022-11-09 17:48:40 +0200

    adding chat feature and auth

--- src/components/SignIn/index.jsx
+++ src/components/SignIn/index.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import { signInWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { auth } from "../../firebase";
+import "../../style.scss";
 
 const SignIn = () => {
-  return (
-    <div>SignIn</div>
-  )
-}
+  const [err, setErr] = useState(false);
+  const navigate = useNavigate();
 
-export default SignIn
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/");
+    } catch (err) {
+      setErr(true);
+    }
+  };
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">7erafy Chat</span>
+        <span className="title">Login</span>
+        <form onSubmit={handleSubmit}>
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+          <button>Sign in</button>
+          {err && <span>Something went wrong</span>}
+        </form>
+        <p>
+          You don't have an account? <Link to="/register">Register</Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
